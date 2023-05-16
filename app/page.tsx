f4f3148593c64dfb82a88c0f064a7471581ab3d9
Martin Reach <marty.reach@gmail.com>
2023-05-16 16:11:50 -0500

    nav and provider, missing api

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
             Discover & Share
             <br className="max-md:hidden"/>
             <span className="orange_gradient text-center">
-                AI-Powered Prompts
+                &nbsp;AI-Powered Prompts
             </span>
         </h1>
         <p className="desc text-center">
